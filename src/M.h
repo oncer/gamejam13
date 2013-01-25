e848f6c94e64a3381f1f8d4fcd87ed54ca52955f
Simon Parzer <simon.parzer@gmail.com>
2013-01-26 00:49:16 +0100

    sounds

--- src/M.h
+++ src/M.h
@@ -6,6 +6,8 @@
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_font.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
 #include <iostream>
 #include <stdexcept>
 #include <map>
