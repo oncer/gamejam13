fe942e78d0880674f7f5fa48c87a01457cab41aa
Getshi <getshi@web.de>
2013-01-27 13:49:53 +0100

    game over fade in

--- src/GameOverBg.cpp
+++ src/GameOverBg.cpp
@@ -4,6 +4,7 @@
 GameOverBg::GameOverBg(void)
 {
 	add_sprite_rect("gameover",0,0,400,240);
+	opacity = 0;
 }
 
 
@@ -15,5 +16,6 @@ void GameOverBg::draw(void) {
 	Sprite::draw();
 }
 void GameOverBg::update(void) {
+	if (opacity < 255) opacity++;
 	Sprite::update();
 }
