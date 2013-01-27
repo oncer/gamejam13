e4881876850e0aaac6639ac20408d9bcf67df076
Getshi <getshi@web.de>
2013-01-27 13:27:23 +0100

    gamestates

---
+++ src/Menu.cpp
@@ -0,0 +1,24 @@
+#include "Menu.h"
+#include "Globals.h"
+
+
+Menu::Menu(void)
+{
+	add_sprite_rect("menu",0,0,400,240);
+}
+
+
+Menu::~Menu(void)
+{
+}
+
+void Menu::draw(void) {
+	Sprite::draw();
+}
+void Menu::update(void) {
+	Sprite::update();
+	if( KBD::IsKeyPressed(KBD::KEY_ACTION)) { 
+		//start game
+		g_game->setGamestate(Game::Gamestate::GAME);
+	}
+}
